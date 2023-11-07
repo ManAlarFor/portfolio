4866aa2cbeeec56df80e7baca98a1b96dd1c19d0
Manuel Alarcon <alarmanuel16@gmail.com>
2023-11-07 09:45:13 +0100

    Halloween

---
+++ Halloween/js/spookyjs.js
@@ -0,0 +1,11 @@
+/***************************************/
+/*         Functions js page           */ 
+/*    Made by Manuel Alarcón Fortes    */ 
+/*              ver:1.0                */ 
+/*                                     */ 
+/***************************************/
+
+function show(){}
+
+
+function hide(){}
